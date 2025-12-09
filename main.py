0af1dad3b248d0236ad1901536a15ca984152054
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-12-09 20:24:41 +0100

    grafics temp

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+if input.button_is_pressed(Button.A):
+    while True:
+        temp = input.temperature()
+        led.plot_bar_graph(temp, 50)
+        serial.write_value("Temperatura", temp)
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
+let temp: number;
+if (input.buttonIsPressed(Button.A)) {
+    while (true) {
+        temp = input.temperature()
+        led.plotBarGraph(temp, 50)
+        serial.writeValue("Temperatura", temp)
+    }
+}
 
