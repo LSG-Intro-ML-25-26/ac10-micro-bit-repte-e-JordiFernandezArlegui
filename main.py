469654a2817c7e9015ab12b7eb525bb3b58f7bcc
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-12-09 20:42:04 +0100

    gota

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-if input.button_is_pressed(Button.A):
-    while True:
-        temp = input.temperature()
-        led.plot_bar_graph(temp, 50)
-        serial.write_value("Temperatura", temp)
+x = 2
+y = 2
+
+while True:
+    if input.button_is_pressed(Button.A):
+        while True:
+            temp = input.temperature()
+            led.plot_bar_graph(temp, 50)
+            serial.write_value("Temperatura", temp)
+            if input.button_is_pressed(Button.B):
+                break
+
+    if input.button_is_pressed(Button.B):
+        led.stop_animation()
+        while True:
+            led.plot(x, y)        
+            basic.pause(50)
+            led.unplot(x, y)
+            
+            accX = input.acceleration(Dimension.X)
+            accY = input.acceleration(Dimension.Y)
+            
+            # Moviment X
+            if accX <= -150 and x > 0:
+                x -= 1
+            elif accX >= 150 and x < 4:
+                x += 1
+            
+            # Moviment Y
+            if accY <= -150 and y > 0:
+                y -= 1
+            elif accY >= 150 and y < 4:
+                y += 1
+            if input.button_is_pressed(Button.A):
+                break
--- main.ts
+++ main.ts
@@ -1,9 +1,48 @@
 let temp: number;
-if (input.buttonIsPressed(Button.A)) {
-    while (true) {
-        temp = input.temperature()
-        led.plotBarGraph(temp, 50)
-        serial.writeValue("Temperatura", temp)
+let accX: number;
+let accY: number;
+let x = 2
+let y = 2
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        while (true) {
+            temp = input.temperature()
+            led.plotBarGraph(temp, 50)
+            serial.writeValue("Temperatura", temp)
+            if (input.buttonIsPressed(Button.B)) {
+                break
+            }
+            
+        }
     }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        led.stopAnimation()
+        while (true) {
+            led.plot(x, y)
+            basic.pause(50)
+            led.unplot(x, y)
+            accX = input.acceleration(Dimension.X)
+            accY = input.acceleration(Dimension.Y)
+            //  Moviment X
+            if (accX <= -150 && x > 0) {
+                x -= 1
+            } else if (accX >= 150 && x < 4) {
+                x += 1
+            }
+            
+            //  Moviment Y
+            if (accY <= -150 && y > 0) {
+                y -= 1
+            } else if (accY >= 150 && y < 4) {
+                y += 1
+            }
+            
+            if (input.buttonIsPressed(Button.A)) {
+                break
+            }
+            
+        }
+    }
+    
 }
-
